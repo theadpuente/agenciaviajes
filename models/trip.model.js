da831a50bec972560d488985fc9a42c60c3b4bbb
Arturo Puente <theadpuente@gmail.com>
2023-01-25 13:30:48 +0100

    ya casi estamos

--- models/trip.model.js
+++ models/trip.model.js
@@ -1,6 +1,6 @@
 const getTrips = () => {
 
-    return db.query('SELECT * FROM appviajes.viajes')
+    return db.query(' SELECT v.id, v.fecha_salida, v.fecha_vuelta, v.ident_ida, v.ident_vuelta, h.nombre as "Hotel", c.nombre as "Cliente" from appviajes.viajes as v, appviajes.hoteles as h, appviajes.clientes as c, appviajes.clientes_has_viajes as chv where v.hoteles_id = h.id AND chv.clientes_id = c.id AND chv.viajes_id = v.id; ')
 
 }
 
@@ -13,9 +13,10 @@ const getTripsbyId = (tripId) => {
 
 const createTrip = ({ fecha_salida, fecha_vuelta, ident_ida, ident_vuelta, hoteles_id }) => {
 
-    return db.query(' insert into viajes (fecha_salida, fecha_vuelta, ident_ida, ident_vuelta, hoteles_id) values (?, ?, ?, ?, ?) ', [fecha_salida, fecha_vuelta, ident_ida, ident_vuelta, hoteles_id])
+    return db.query(' insert into viajes (fecha_salida, fecha_vuelta, ident_ida, ident_vuelta, hoteles_id) values (?, ?, ?, ?, ?)', [fecha_salida, fecha_vuelta, ident_ida, ident_vuelta, hoteles_id])
 
 };
+
 
 
 const updateTripbyId = (tripId, { fecha_salida, fecha_vuelta, ident_ida, ident_vuelta, hoteles_id }) => {
@@ -29,6 +30,9 @@ const deleteTripbyId = (tripId) => {
     return db.query('DELETE FROM viajes WHERE id = ?', [tripId])
 
 }
+
+
+
 
 
 
